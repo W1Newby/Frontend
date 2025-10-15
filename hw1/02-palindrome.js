02cbf9420f8d0ee42a7f4dc6fff7e07e8778f069
W1Newby <newby@pdx.edu>
2025-10-15 08:35:08 -0700

    update flexbox on my github site?

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,3 +1,12 @@
-const elem = document.querySelector('input');
+//const elem = document.querySelector('input');
 
-elem.addEventListener('input', handleInput);
+//elem.addEventListener('input', handleInput);
+
+const input = document.getElementById("number-input");
+const result = document.getElementById("result");
+
+input.addEventListener("input", () => {
+  result.textContent = `You typed: ${input.value}`;
+});
+
+console.log("JS file loaded ✅");
